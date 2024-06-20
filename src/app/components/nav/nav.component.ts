99ddd969a766d906c888b2dee07eefa52e69ea18
JoaoNeto4 <joaoquatlo@hotmail.com>
2024-06-19 22:23:29 -0300

    Impl logout

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Toast, ToastrService } from 'ngx-toastr';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-nav',
@@ -9,10 +11,19 @@ import { Router } from '@angular/router';
 export class NavComponent implements OnInit {
   showFiller = false;
 
-  constructor(private router: Router) { }
+  constructor(
+    private router: Router,
+    private authService: AuthService,
+    private toast: ToastrService) { }
 
   ngOnInit(): void {
     this.router.navigate(['tecnicos'])
+  }
+
+  logout(){
+    this.router.navigate(['login'])
+    this.authService.logout();
+    this.toast.info('Logout realizado com sucesso', 'Logout', {timeOut: 4000})
   }
 
 }
