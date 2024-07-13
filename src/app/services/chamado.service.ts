cdafbc3826629ca71e9c8e20f399663664a81400
JoaoNeto4 <joaoquatlo@hotmail.com>
2024-07-12 21:21:46 -0300

    Impl update Chamado

--- src/app/services/chamado.service.ts
+++ src/app/services/chamado.service.ts
@@ -15,9 +15,17 @@ export class ChamadoService {
     return this.http.get<Chamado[]>(`${API_CONFIG.base_url}/chamados`);
   }
 
+  findById(id: any): Observable<Chamado>{
+    return this.http.get<Chamado>(`${API_CONFIG.base_url}/chamados/${id}`);
+  }
+
   create(chamado: Chamado): Observable<Chamado> {
     return this.http.post<Chamado>(`${API_CONFIG.base_url}/chamados`, chamado)
   }
 
+  update(chamado: Chamado): Observable<Chamado> {
+    console.log(chamado);
+    return this.http.put<Chamado>(`${API_CONFIG.base_url}/chamados/${chamado.id}`, chamado)
+  }
 
 }
